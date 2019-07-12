22045e66ab4d9e545fac5d4ce9fa436df0934a0a
Gabe <42557432+spookypants@users.noreply.github.com>
2019-07-11 23:16:42 -0500

    orm creation

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,6 @@
 var connection = require("../config/connection.js");
 
-function printQuestionMarks(num) {
+function createQmarks(num) {
     var arr = [];
     for (var i = 0; i < num; i++) {
         arr.push("?");
@@ -8,11 +8,11 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
-function objToSql(ob) {
+function translateSql(ob) {
     var arr = [];
     for (var key in ob) {
         var value = ob[key];
-        if (Object.hasOwnPropert.call(ob, key)) {
+        if (Object.hasOwnProperty.call(ob, key)) {
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             }
@@ -23,5 +23,43 @@ function objToSql(ob) {
 }
 
 var orm = {
-    selectAll: function()
-}
+    selectAll: function(table, cb){
+        var dbQuery = "SELECT * FROM " + table + ";";
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        })
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var dbQuery = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + createQmarks(vals.length) + ") ";
+        console.log(dbQuery);
+        connection.query(dbQuery, vals, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var dbQuery = "UPDATE " + table + " SET" + translateSql(objColVals) + " WHERE " + condition;
+        console.log(dbQuery);
+        connection.query(dbQuery, vals, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    deleteOne: function(table, condition, db) {
+        var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+        console.log(dbQuery);
+        connection.query(dbQuery, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+};
